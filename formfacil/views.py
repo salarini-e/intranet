0cb03f8b5ec0b7afd23625c2e8bebf12c879e469
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2024-03-13 11:18:31 -0300

    13.03.24.ED - FormFácíl

--- formfacil/views.py
+++ formfacil/views.py
@@ -6,6 +6,19 @@ def IndicacaoComitePSP(request):
         form = FormIndicacaoComitePSPForm(request.POST)
         if form.is_valid():
             form.save()
+            context = {
+                'titulo': 'Formulário para indicação de representante no Comitê de Padronização dos Sistemas Públicos Municipais',    
+                'subtitulo': 'Subsecretaria de TI',
+                'mensagem': "<span class='text-success'>Formulário enviado com sucesso!</span> Estamos entusiasmados com a oportunidade de colaborar com você e aproveitar ao máximo sua valiosa contribuição."
+            }
+            return render(request, 'formfacil/formfacil_success.html', context)
     else:
         form = FormIndicacaoComitePSPForm()
-    return render(request, 'formfacil/formfacil_indicacao_comite.html', {'form': form})
+    context = {
+            'form': form,
+            'titulo': 'Formulário para indicação de representante no Comitê de Padronização dos Sistemas Públicos Municipais',
+            'subtitulo': 'Subsecretaria de TI',
+            'mensagem': 'Agradecemos antecipadamente pela sua disposição em servir como representante no Comitê de Padronização dos Sistemas Públicos Municipais. Seu comprometimento e expertise serão fundamentais para impulsionar melhorias significativas em nossos sistemas.'
+        }
+    return render(request, 'formfacil/formfacil_form.html', context)
+
