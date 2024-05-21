d26f19a0f11f4009600fe6579a28abc96140b3a9
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2024-05-21 10:24:37 -0300

    21.05.24.1-3.ED

--- softwares/views.py
+++ softwares/views.py
@@ -3,17 +3,17 @@ from django.http import FileResponse, HttpResponse
 from .models import *
 
 def sistemas(request):
-    tipos = TipoSistema.objects.all()
+    tipos = TipoSistema.objects.all().order_by('nome')
     return render(request, 'softwares/sistemas/index.html', context={'tipos': tipos})
 
 def sistemas_listar(request, slug):
     tipo = get_object_or_404(TipoSistema, slug=slug)
-    sistemas = Sistemas.objects.filter(tipo=tipo)
+    sistemas = Sistemas.objects.filter(tipo=tipo).order_by('nome')
     return render(request, 'softwares/sistemas/listar.html', context={'sistemas': sistemas, 'tipo': tipo})
     
 def downloads(request):
-    softwares = Downloads.objects.all()
-    tipos = TipoDownload.objects.all()
+    softwares = Downloads.objects.all().order_by('nome')
+    tipos = TipoDownload.objects.all().order_by('nome')
     return render(request, 'softwares/downloads/index.html', context={'softwares': softwares, 'tipos': tipos})
 
 def download_file(request, software_id):
