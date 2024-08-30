0e4840a9d2c18528d83dacca1f875c15b81e621c
DiogoCardinot <diogocardinotuerj@gmail.com>
2024-08-30 14:19:46 -0300

    30.08.2024.03.DI

--- formfacil/admin.py
+++ formfacil/admin.py
@@ -7,4 +7,19 @@ admin.site.register(FormSugestaoSemanaNacionalCET2024)
 admin.site.register(Sistemas_EL)
 admin.site.register(CadastroEL)
 admin.site.register(Opcao_Turmas)
-admin.site.register(Cadastro_Aulas_Processo_Digital)
+
+class CadastroAulasProcessoDigitalAdmin(admin.ModelAdmin):
+    # Campos que serão exibidos na lista de registros
+    list_display = ('nome', 'matricula', 'secretaria', 'setor', 'telefone', 'turma_escolhida', 'dt_registro')
+    
+    # Campos pelos quais você pode buscar
+    search_fields = ('nome', 'matricula', 'telefone', 'secretaria', 'setor')
+    
+    # Filtros laterais para facilitar a busca
+    list_filter = ('turma_escolhida')
+    
+    # Ordem padrão dos registros
+    ordering = ('-dt_registro', 'nome')  # Exibe os registros mais recentes primeiro
+
+# Registro do modelo no admin
+admin.site.register(Cadastro_Aulas_Processo_Digital, CadastroAulasProcessoDigitalAdmin)
