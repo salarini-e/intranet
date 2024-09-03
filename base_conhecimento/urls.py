5995cdd360ae208148e4f847eca888fc134104ed
DiogoCardinot <diogocardinotuerj@gmail.com>
2024-09-03 14:17:31 -0300

    03.09.2024.02.DI renderiza o video do drive de acordo com o subtopico e permite o download dos videos

--- base_conhecimento/urls.py
+++ base_conhecimento/urls.py
@@ -1,10 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name='base_conhecimento'
+app_name = 'base_conhecimento'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('subtopicos/<int:topico_id>/', views.subtopicos, name='subtopicos'),
-
+    path('subtopicos/<int:topico_id>/<int:subtopico_id>', views.displayVideo, name='display_video'),
+    path('download-video/<int:topico_id>/<int:subtopico_id>/', views.download_video, name='download_video'),
 ]
--- base_conhecimento/views.py
+++ base_conhecimento/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Topico, Subtopico
+from .models import Topico, Subtopico, Arquivo_GoogleDrive
+from django.contrib.auth.decorators import login_required
+import requests
+from django.http import StreamingHttpResponse, HttpResponse
+import re
 
 # Create your views here.
+@login_required
 def index(request):
     topicos = Topico.objects.filter(ativo= True)
     context = {
@@ -9,6 +14,7 @@ def index(request):
     }
     return render(request, 'base_conhecimento/index.html', context)
 
+@login_required
 def subtopicos(request, topico_id):
     # Obter o tópico selecionado ou retornar um erro 404 se não existir
     topico = get_object_or_404(Topico, id=topico_id, ativo=True)
@@ -21,3 +27,69 @@ def subtopicos(request, topico_id):
         'subtopicos': subtopicos
     }
     return render(request, 'base_conhecimento/subtopicos.html', context)
+
+@login_required
+def displayVideo(request, topico_id, subtopico_id):
+    # Obter o tópico e o subtopico selecionados ou retornar um erro 404 se não existirem
+    topico = get_object_or_404(Topico, id=topico_id, ativo=True)
+    subtopico = get_object_or_404(Subtopico, id=subtopico_id, topico=topico)
+    
+    # Filtrar o vídeo associado ao subtopico
+    video = Arquivo_GoogleDrive.objects.filter(subtopico=subtopico).first()
+
+    context = {
+        'topico': topico,
+        'subtopico': subtopico,
+        'video': video,
+    }
+    return render(request, 'base_conhecimento/display_video.html', context)
+
+@login_required
+def download_video(request, topico_id, subtopico_id):
+    # Obter o tópico e o subtopico selecionados ou retornar um erro 404 se não existirem
+    topico = get_object_or_404(Topico, id=topico_id, ativo=True)
+    subtopico = get_object_or_404(Subtopico, id=subtopico_id, topico=topico)
+    
+    # Filtrar o vídeo associado ao subtopico
+    video = get_object_or_404(Arquivo_GoogleDrive, subtopico=subtopico)
+    
+    # Extrair a URL do iframe HTML
+    iframe_html = video.iframe
+    download_url = extract_video_url_from_iframe(iframe_html)
+    
+    if not download_url:
+        return HttpResponse("Não foi possível encontrar a URL do vídeo.", status=404)
+
+    # Baixar o vídeo em streaming
+    response = requests.get(download_url, stream=True)
+    response.raise_for_status()  # Verifica se ocorreu um erro
+    
+     # Criar o nome do arquivo para o download
+    file_name = f"{topico.nome}-{subtopico.tema}.mp4"
+    # Criar uma resposta HTTP com o conteúdo do vídeo
+    http_response = StreamingHttpResponse(response.iter_content(chunk_size=8192), content_type='application/octet-stream')
+    http_response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+    
+    return http_response
+
+
+def extract_video_url_from_iframe(iframe_html):
+    """
+    Extrai a URL do vídeo do HTML do iframe e converte o link de preview
+    do Google Drive para um link de download direto.
+    """
+    match = re.search(r'src="([^"]+)"', iframe_html)
+    if match:
+        preview_url = match.group(1)
+        return convert_to_download_url(preview_url)
+    return None
+
+def convert_to_download_url(preview_url):
+    """
+    Converte a URL de preview do Google Drive para um link de download direto.
+    """
+    # Extraia o ID do arquivo do URL de preview
+    file_id = re.search(r'/d/([^/]+)/', preview_url)
+    if file_id:
+        return f'https://drive.google.com/uc?export=download&id={file_id.group(1)}'
+    return None
