3fb2c8ab572726be9c812703c33e39b65f56863e
Eduardo Salarini <80537456+salarini-e@users.noreply.github.com>
2025-02-06 11:33:12 -0300

    Update custom_filters.py

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -110,7 +110,7 @@ def acessoPonto(servidor):
 
     is_responsavel = Responsavel.is_responsavel(servidor.user) 
     # has_controle_de_ponto = Responsavel.objects.filter(user=servidor.user).exists()
-    value = is_responsavel or servidor.setor.id == 4
+    value = is_responsavel or servidor.setor.id == 5
     return value
 
 @register.filter
