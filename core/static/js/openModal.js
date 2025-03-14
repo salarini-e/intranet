57c729cb4a0fd497cfa49f96dba0416239d2a8aa
salarini-e <eduardo.pmnf@gmail.com>
2025-03-14 14:27:10 -0300

    att

--- core/static/js/openModal.js
+++ core/static/js/openModal.js
@@ -1,24 +1,51 @@
+// const openModalBtn = document.getElementById('openModal');
+// const closeModalBtns = document.querySelectorAll('#myModal .btn-close');
+
+// const modal = document.getElementById('myModal');
+// if (openModalBtn){
+//     openModalBtn.addEventListener('click', () => {
+//         modal.style.display = 'block';
+//     });    
+// }
+
+// closeModalBtns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         modal.style.display = 'none';
+//     });
+// });
+
+// function openModal(id){
+//     const modal = document.getElementById(id);
+//     modal.style.display = 'block';
+// }
+
+// function closeModal(id){
+//     const modal = document.getElementById(id);
+//     modal.style.display = 'none';
+// }
+
 const openModalBtn = document.getElementById('openModal');
 const closeModalBtns = document.querySelectorAll('#myModal .btn-close');
-
 const modal = document.getElementById('myModal');
 
-openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
-
-closeModalBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        modal.style.display = 'none';
-    });
-});
-
-function openModal(id){
-    const modal = document.getElementById(id);
-    modal.style.display = 'block';
+if (openModalBtn && modal) {
+    openModalBtn.addEventListener('click', () => openModal('myModal'));
 }
 
-function closeModal(id){
+closeModalBtns.forEach(btn => {
+    btn.addEventListener('click', () => closeModal('myModal'));
+});
+
+function openModal(id) {
     const modal = document.getElementById(id);
-    modal.style.display = 'none';
+    if (modal) {
+        modal.style.display = 'block';
+    }
+}
+
+function closeModal(id) {
+    const modal = document.getElementById(id);
+    if (modal) {
+        modal.style.display = 'none';
+    }
 }
