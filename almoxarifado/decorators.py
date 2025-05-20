ceca880f56d67263c9c6bccc5c215f729f8104b0
gustavodias <gustavou.dias@gmail.com>
2025-05-20 13:53:29 -0300

    add almoxarifado

--- almoxarifado/decorators.py
+++ almoxarifado/decorators.py
@@ -11,7 +11,7 @@ def required_almoxarifado(view_func):
 
         # AQUI EU LIMITO O ACESSO A PARTE DE ALMOXARIFADO APENAS AO ROMULO E ALESSANDRO DA SUBSECRETARIA DE TI
         # SERÁ UTILIZADO ESSE DECORATORS NA MODELS DA SEGUINTE FORMA: @required_almoxarifado
-        if funcionario.exists() and ((funcionario.first().username == '063613') or (funcionario.first().username == '063563') or (funcionario.first().username == 'gustavo')):
+        if funcionario.exists() and ((funcionario.first().username == '063613') or (funcionario.first().username == '063563') or (funcionario.first().username == 'gustavo.dias')):
             
             return view_func(request, *args, **kwargs)
         return HttpResponseForbidden("Acesso restrito a Secretaria de TI.")
