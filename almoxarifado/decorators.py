9c8eedfc32de842a9392cd05472d3154d13db184
Eduardo Salarini <luis.salarini@aluno.cefet-rj.br>
2025-06-18 16:01:13 -0300

    att

--- almoxarifado/decorators.py
+++ almoxarifado/decorators.py
@@ -10,7 +10,7 @@ def required_almoxarifado(view_func):
 
         # AQUI EU LIMITO O ACESSO A PARTE DE ALMOXARIFADO APENAS AO ROMULO E ALESSANDRO DA SUBSECRETARIA DE TI
         # SERÁ UTILIZADO ESSE DECORATORS NA MODELS DA SEGUINTE FORMA: @required_almoxarifado
-        if funcionario.exists() and ((funcionario.first().matricula == '063613') or (funcionario.first().matricula == '063563') or (funcionario.first().matricula == 'gustavo.dias')):
+        if funcionario.exists() and ((funcionario.first().matricula == '063613') or (funcionario.first().matricula == '063563') or (funcionario.first().matricula == 'gustavo.dias') or (funcionario.first().matricula == '063508')):
             
             return view_func(request, *args, **kwargs)
         return HttpResponseForbidden("Acesso restrito a Secretaria de TI.")
